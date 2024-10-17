16d0008fc2be6891cf127e737121639bf3e3ae09
ippa96 <96ippa@gmail.com>
2024-10-17 14:52:06 +0300

    nothing basicly

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,15 @@
 from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
+
 @auth.route('/login')
 def login():
-    return "<>"
+    return "<p>Login</p>" 
+
+@auth.rutine('/logout')
+def logout():
+    return"<p>Logout</p>" 
+
+@auth.route('/sign-up')
+def logout():
+    return "<p>sign up</p>" 
